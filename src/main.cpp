fa6ca222be9c46aab08a7bcc80da59a56ee0dce4
K10-K10 <keito.tottori@gmail.com>
2025-12-30 01:01:15 +0900

    update cmake

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include <K10-K10/terminal.h>
+
+using namespace terminal;
+int main() {
+  app.init();
+  Text text;
+  text.contents("Hello TUI!");
+
+  List list;
+  list.items({"item1", "item2", "item3", "item4", "item5"});
+  Block box;
+  text.position({1, 1, 20, 1});
+  list.position({1, 3, 20, 5});
+  box.position({0, 0, FULL, FULL});
+  app.loop([&]() {
+    box.draw();
+    text.draw();
+    list.draw();
+
+    input::key.read();
+    auto key = input::key.getKeyCode();
+
+    if (key == input::KeyCode::UP) {
+      list.move_up();
+    }
+    if (key == input::KeyCode::DOWN) {
+      list.move_down();
+    }
+    if (key == input::KeyCode::CHAR) {
+      char c = input::key.getCurrentChar();
+      if (c == 'q') {
+        app.stop();
+      }
+    }
+  });
+
+  return 0;
+}
+
